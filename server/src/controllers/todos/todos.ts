c34ab03a1240fabd5aa1b8bad9153a9cd321787c
IsacVera <isaacvera37@icloud.com>
2023-11-03 02:31:53 -0700

    add controllers for todos

---
+++ server/src/controllers/todos/todos.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express"
+import { ITodo } from "../../types/todo"
+import Todo from "../../models/todo"
+
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+    try{ 
+        const todos: ITodo[] = await Todo.find();
+        res.status(200).json({ todos });
+    } catch (error) {
+        throw (error);
+    }
+}
+
+const addTodo = async (req:Request, res: Response): Promise<void> => {
+    try{
+        const body = req.body as Pick<ITodo, "title" | "description" | "status">;
+
+        const todo: ITodo = new Todo({
+            name: body.title,
+            description: body.description,
+            status: body.status,
+        })
+
+        const newTodo: ITodo = await todo.save();
+        const allTodos: ITodo[] = await Todo.find();
+
+        res
+            .status(201)
+            .json({ message: "Todo added", todo: newTodo, todos: allTodos});
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+const updateTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body,
+        } = req;
+
+        const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+            {_id: id},
+            body
+        );
+        
+        const allTodos: ITodo[] = await Todo.find();
+        res.status(200).json({
+            message: "Todos updated",
+            todo: updateTodo,
+            todos: allTodos,
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+            req.params.id
+        )
+
+        const allTodos: ITodo[] = await Todo.find();
+        res.status(200).json({
+            message: "Todo deleted",
+            todo: deletedTodo,
+            todos: allTodos,
+        })
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { getTodos, addTodo, updateTodo, deleteTodo };
